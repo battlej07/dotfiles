b95018ebdfa343b9919bbf022525a7337b147c64
Jamie Blättler <jamie.blaettler@proton.me>
2026-04-30 21:09:30 +0200

    feat: add fish configuration

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,27 @@
+set -Ux EDITOR nvim
+set -Ux VISUAL nvim
+
+if status is-interactive
+end
+
+function fish_greeting
+    fastfetch
+end
+
+function starship_transient_prompt_func
+    starship module character
+end
+starship init fish | source
+enable_transience
+
+alias ls='eza -al --color=always --group-directories-first --icons'
+alias la='eza -a --color=always --group-directories-first --icons'
+alias ll='eza -l --color=always --group-directories-first --icons'
+alias lt='eza -aT --color=always --group-directories-first --icons'
+alias l.="eza -a | grep -e '^\.'"
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
